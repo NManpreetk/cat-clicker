f828fba4ba51b1f1c11d92b6be22f83e36cf4ad9
Manpreet kaur <manpreetmanku26@gmail.com>
2018-05-17 10:32:27 -0500

    small changes made

--- myscript.js
+++ myscript.js
@@ -1,7 +1,8 @@
 var b = document.querySelector("#cat")
-var c = document.querySelector("#counter")
+var c = document.querySelector("#count")
 
 let counting = 0;
 b.addEventListener("click", function(){
     c.textContent = counting++;
+    b.innerHTML = ("https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA5Ny85NTkvb3JpZ2luYWwvc2h1dHRlcnN0b2NrXzYzOTcxNjY1LmpwZw==")
 })
